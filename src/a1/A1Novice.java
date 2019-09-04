374838b8b0f179544f0a25993256a8003039afaf
adi sankar <absankar@live.unc.edu>
2019-09-03 21:53:56 -0400

    working novice

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -7,8 +7,36 @@ public class A1Novice {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
+		
+		int customer_count = scan.nextInt(); //3
+		 
+		int i = 0;
+		do {
+			String first_name = scan.next(); //Carrie
+			String last_name = scan.next();  //Brownstein
+			int num_of_items = scan.nextInt(); //3 items
+			double sum = 0.0;
+			
+			
+			for (int z=0; z < num_of_items; z++) {
+				int quant_of_items = scan.nextInt();
+				String item_name = scan.next();
 
-		// Your code follows here.
+				double price_of_item = scan.nextDouble();
+				//System.out.println("price" + price_of_item);
+
+				double cost = quant_of_items * price_of_item; 
+				sum += cost; //9.05
+			}
+			char first_letter = first_name.charAt(0);
+			System.out.println(first_letter + ". " + last_name + ": " + sum);
+			
+			i++;
+			
+		}
+		while (i < customer_count);
+		scan.close();	
+		
 		
 	}
 }
